6c14638a2e66f1a22f91a8f04656746ccc5cb7cb
JamesTrewern <trewern.james@gmail.com>
2024-07-01 20:13:49 +0100

    fixed tests

--- src/examples.rs
+++ src/examples.rs
@@ -1,117 +1,116 @@
-// // //Broad test on example files to prove working state of application
+// //Broad test on example files to prove working state of application
 
-// use std::collections::HashMap;
+use std::collections::HashMap;
 
-// use crate::{
-//     heap::store::Store,
-//     interface::{
-//         parser::{parse_goals, tokenise},
-//         state::State,
-//     },
-//     program::program::DynamicProgram,
-//     resolution::solver::Proof,
-// };
+use crate::{
+    heap::store::Store,
+    interface::{
+        parser::{parse_goals, tokenise},
+        state::State,
+    },
+    program::program::DynamicProgram,
+    resolution::solver::Proof,
+};
 
-// fn setup<'a>(file: &str, goal: &str) -> (State, Vec<usize>) {
-//     let mut state = State::new(None);
-//     state.load_file(file).unwrap();
+fn setup<'a>(file: &str) -> State {
+    let state = State::new(None);
+    state.load_file(file).unwrap();
+    state
+}
 
-//     let goals: Vec<usize> = parse_goals(&tokenise(goal))
-//         .unwrap()
-//         .into_iter()
-//         .map(|t| t.build_to_heap(&mut store, &mut HashMap::new(), false))
-//         .collect();
-//     state.to_static_heap(&mut store);
-//     (state)
-// }
+fn make_goals<'a>(state: &'a State, goals: &str) -> (Vec<usize>, Store<'a>) {
+    let mut store = Store::new(state.heap.try_read_slice().unwrap());
+    let goals: Vec<usize> = parse_goals(&tokenise(goals))
+        .unwrap()
+        .into_iter()
+        .map(|t| t.build_to_heap(&mut store, &mut HashMap::new(), false))
+        .collect();
+    (goals, store)
+}
+#[test]
+fn ancestor() {
+    let state = setup("./examples/family");
+    let (goals, store) = make_goals(&state, "ancestor(ken,james), ancestor(christine,james).");
+
+    let proof = Proof::new(
+        &goals,
+        store,
+        DynamicProgram::new(None, state.program.read().unwrap()),
+        None,
+        &state,
+    );
+
+    println!("proof");
+    let mut proofs = 0;
+    for _ in proof {
+        proofs += 1;
+    }
+    assert!(proofs > 0);
+}
+
+#[test]
+fn map() {
+    let state = setup("./examples/map");
+
+    let (goals, store) = make_goals(&state, "map([1,2,3],[2,4,6], X).");
+    let proof = Proof::new(
+        &goals,
+        store,
+        DynamicProgram::new(None, state.program.read().unwrap()),
+        None,
+        &state,
+    );
+
+    let mut proofs = 0;
+    for _ in proof {
+        proofs += 1;
+    }
+    assert!(proofs > 0);
+}
+
+#[test]
+fn odd_even() {
+    let state = setup("./examples/odd_even");
+
+    let (goals, store) = make_goals(&state, "even(4), not(even(3)).");
+
+    let proof = Proof::new(
+        &goals,
+        store,
+        DynamicProgram::new(None, state.program.read().unwrap()),
+        None,
+        &state,
+    );
+
+    let mut proofs = 0;
+    for _ in proof {
+        proofs += 1;
+    }
+    assert!(proofs > 0);
+}
 
 // #[test]
-// fn ancestor() {
-//     let (state,goals) = setup(
-//         "./examples/family",
-//         "ancestor(ken,james), ancestor(christine,james).",
-//     );
-//     let store = Store::new(state.heap.read_slice().unwrap());
+// fn move_up(){
+//     let mut state = State::new(Some(
+//         Config::new().max_h_clause(0).max_h_preds(0).debug(true).max_depth(10),
+//     ));
 
-//     let proof = Proof::new(
-//         &goals,
-//         store,
-//         DynamicProgram::new(None, state.program.read().unwrap()),
-//         None,
-//         &state
-//     );
+//     state.load_file("./examples/move_up");
 
+//     state.prog.print_prog(&state.heap);
 
-//     println!("proof");
+//     let goals: Vec<usize> = parse_goals(&tokenise("move_up([1,5],[1,6])."))
+//     .unwrap()
+//     .into_iter()
+//     .map(|t| t.build_on_heap(&mut state.heap, &mut HashMap::new()))
+//     .collect();
+
+//     let proof = Proof::new(&goals, &mut state);
+
 //     let mut proofs = 0;
-//     for _ in proof {
+//     for branch in proof {
 //         proofs += 1;
 //     }
+
 //     assert!(proofs > 0);
 // }
-
-// #[test]
-// fn map() {
-//     let (state,goals) = setup("./examples/map", "map([1,2,3],[2,4,6], X).");
-//     let store = Store::new(state.heap.read_slice().unwrap());
-    
-//     let proof = Proof::new(
-//         &goals,
-//         store,
-//         DynamicProgram::new(None, state.program.read().unwrap()),
-//         None,
-//         &state
-//     );
-    
-//     let mut proofs = 0;
-//     for _ in proof {
-//         proofs += 1;
-//     }
-//     assert!(proofs > 0);
-// }
-
-// #[test]
-// fn odd_even() {
-//     let (state,goals) = setup("./examples/odd_even", "even(4), not(even(3)).");
-//     let store = Store::new(state.heap.read_slice().unwrap());
-
-//     let proof = Proof::new(
-//         &goals,
-//         store,
-//         DynamicProgram::new(None, state.program.read().unwrap()),
-//         None,
-//         &state
-//     );
-    
-//     let mut proofs = 0;
-//     for _ in proof {
-//         proofs += 1;
-//     }
-//     assert!(proofs > 0);
-// }
-
-// // #[test]
-// // fn move_up(){
-// //     let mut state = State::new(Some(
-// //         Config::new().max_h_clause(0).max_h_preds(0).debug(true).max_depth(10),
-// //     ));
-
-// //     state.load_file("./examples/move_up");
-
-// //     state.prog.print_prog(&state.heap);
-
-// //     let goals: Vec<usize> = parse_goals(&tokenise("move_up([1,5],[1,6])."))
-// //     .unwrap()
-// //     .into_iter()
-// //     .map(|t| t.build_on_heap(&mut state.heap, &mut HashMap::new()))
-// //     .collect();
-
-// //     let proof = Proof::new(&goals, &mut state);
-
-// //     let mut proofs = 0;
-// //     for branch in proof {
-// //         proofs += 1;
-// //     }
-
-// //     assert!(proofs > 0);
-// // }
