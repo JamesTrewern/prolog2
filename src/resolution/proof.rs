fdce7bd98bc2e442eb9d2e882ab778057d4ee1c7
JamesTrewern <trewern.james@gmail.com>
2025-07-09 02:57:08 +0100

    create proof module with data structure layout

---
+++ src/resolution/proof.rs
@@ -0,0 +1,42 @@
+use crate::{
+    heap::query_heap::QueryHeap,
+    program::{clause::Clause, hypothesis::Hypothesis},
+};
+
+pub type Binding = (usize, usize);
+
+pub(super) struct Env {
+    pub(super) goal: usize, // Pointer to heap literal
+    pub(super) bindings: Vec<Binding>,
+    pub(super) choices: Vec<Clause>, //Array of choices which have not been tried
+    pub(super) new_clause: bool,     //Was a new clause created by this enviroment
+    pub(super) invent_pred: bool,    //If there was a new clause was a new predicate symbol invented
+    pub(super) children: usize,      //How many child goals were created
+    pub(super) depth: usize,
+}
+
+impl Env {
+    pub fn new(goal: usize, depth: usize) -> Self {
+        Env {
+            goal,
+            bindings: Vec::new(),
+            choices: Vec::new(),
+            new_clause: false,
+            invent_pred: false,
+            children: 0,
+            depth,
+        }
+    }
+}
+
+struct Proof<'a> {
+    stack: Vec<Env>,
+    pointer: usize,
+    hypothesis: Hypothesis,
+    heap: QueryHeap<'a>,
+    root_goals: u8, //How many goals were in initial query
+}
+
+impl<'a> Proof<'a> {
+    // pub fn new();
+}
