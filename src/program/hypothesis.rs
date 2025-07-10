050aaf2cc69819d462ff6bcbb9c8593ed32d7683
JamesTrewern <trewern.james@gmail.com>
2025-07-10 14:56:02 +0100

    Implemented simple hypothesis functionality

--- src/program/hypothesis.rs
+++ src/program/hypothesis.rs
@@ -1,6 +1,33 @@
+use std::ops::Deref;
+
 use super::clause::Clause;
 
-pub struct Hypothesis {
-    clauses: Vec<Clause>,
-    n_inv_predicate: u8,
+pub struct Hypothesis (
+    Vec<Clause>
+);
+
+impl Hypothesis {
+    pub fn new() -> Self{
+        Hypothesis(Vec::new())
+    }
+
+    pub fn len(&self) -> usize{
+        self.0.len()
+    }
+
+    pub fn push_clause(&mut self, clause: Clause){
+        self.0.push(clause);
+    }
+
+    pub fn drop_clause(&mut self){
+        self.0.pop().unwrap().drop();
+    }
+}
+
+impl Deref for Hypothesis {
+    type Target = Vec<Clause>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
 }
