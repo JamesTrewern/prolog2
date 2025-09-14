792ea07a50fb3857c0c3b9e108c6c512ff1b0a0a
JamesTrewern <trewern.james@gmail.com>
2025-09-14 12:54:02 +0100

    updated example files

--- examples/odd_even.pl
+++ examples/odd_even.pl
@@ -1,17 +1,11 @@
-
-% Configuration options
-:- body_pred(zero,1), body_pred(prev,2), max_h_clause(3), max_h_preds(1), max_depth(5), debug(false).
-
-% Meta clauses
-P(X):- Q(X,Y), R(Y), {X,Y}.
-P(X):- Q(X), {X,Y}.
-
-% Background Knowledge
 zero(0).
 prev(1,0).
 prev(2,1).
 prev(3,2).
 prev(4,3).
+
+P(X):- Q(X,Y), R(Y) {P,Q,R}.
+P(X):- Q(X) {P,Q}.
 
 
 % ?- even(4), not(even(3))
