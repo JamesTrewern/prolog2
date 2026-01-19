821c7278d4d49e03d4bb22a51a2e4f76c9f16f5a
James Trewern <trewern.james@gmail.com>
2026-01-19 11:35:09 +0000

    delete old code

--- src/old/old_heap/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod store;
-pub mod heap;
-pub mod symbol_db;
-pub mod branch_store;
-#[cfg(test)]
-mod tests;
--- src/old/old_resolution/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod solver;
-pub mod unification;
-mod call;
-mod build;
-mod proof_stack;
-
-// #[cfg(test)]
-// mod tests;
-
