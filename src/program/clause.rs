bfcb5e205a5c2c2ce10dfb03021b030318739ebd
James Trewern <trewern.james@gmail.com>
2026-03-26 15:06:06 +0000

    facts include .

--- src/program/clause.rs
+++ src/program/clause.rs
@@ -93,7 +93,7 @@ impl Clause {
 
     pub fn to_string(&self, heap: &impl Heap) -> String {
         if self.len() == 1{
-            return heap.term_string(self.head())
+            return heap.term_string(self.head()) + "."
         }
         let mut buffer = format!("{}:-", heap.term_string(self.head()));
         for body_literal in self.body() {
