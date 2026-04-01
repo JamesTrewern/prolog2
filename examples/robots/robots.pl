a2e069a715400bf5b84303a58c0b7f08c3390208
James Trewern <trewern.james@gmail.com>
2026-04-01 15:09:20 +0100

    fix robots example

--- examples/robots/robots.pl
+++ examples/robots/robots.pl
@@ -90,3 +90,110 @@ move_down_then_right(Ss,Gs):-
 move_down_then_left(Ss,Gs):-
 	move_down(Ss,Ss_2),
 	move_left(Ss_2,Gs).
+
+location(0,0).
+location(0,1).
+location(0,2).
+location(0,3).
+location(0,4).
+location(1,0).
+location(1,1).
+location(1,2).
+location(1,3).
+location(1,4).
+location(2,0).
+location(2,1).
+location(2,2).
+location(2,3).
+location(2,4).
+location(3,0).
+location(3,1).
+location(3,2).
+location(3,3).
+location(3,4).
+location(4,0).
+location(4,1).
+location(4,2).
+location(4,3).
+location(4,4).
+
+move_down([0/1,G,4 - 4],[0/0,G,4 - 4]).
+move_down([0/2,G,4 - 4],[0/1,G,4 - 4]).
+move_down([0/3,G,4 - 4],[0/2,G,4 - 4]).
+move_down([0/4,G,4 - 4],[0/3,G,4 - 4]).
+move_down([1/1,G,4 - 4],[1/0,G,4 - 4]).
+move_down([1/2,G,4 - 4],[1/1,G,4 - 4]).
+move_down([1/3,G,4 - 4],[1/2,G,4 - 4]).
+move_down([1/4,G,4 - 4],[1/3,G,4 - 4]).
+move_down([2/1,G,4 - 4],[2/0,G,4 - 4]).
+move_down([2/2,G,4 - 4],[2/1,G,4 - 4]).
+move_down([2/3,G,4 - 4],[2/2,G,4 - 4]).
+move_down([2/4,G,4 - 4],[2/3,G,4 - 4]).
+move_down([3/1,G,4 - 4],[3/0,G,4 - 4]).
+move_down([3/2,G,4 - 4],[3/1,G,4 - 4]).
+move_down([3/3,G,4 - 4],[3/2,G,4 - 4]).
+move_down([3/4,G,4 - 4],[3/3,G,4 - 4]).
+move_down([4/1,G,4 - 4],[4/0,G,4 - 4]).
+move_down([4/2,G,4 - 4],[4/1,G,4 - 4]).
+move_down([4/3,G,4 - 4],[4/2,G,4 - 4]).
+move_down([4/4,G,4 - 4],[4/3,G,4 - 4]).
+move_left([1/0,G,4 - 4],[0/0,G,4 - 4]).
+move_left([1/1,G,4 - 4],[0/1,G,4 - 4]).
+move_left([1/2,G,4 - 4],[0/2,G,4 - 4]).
+move_left([1/3,G,4 - 4],[0/3,G,4 - 4]).
+move_left([1/4,G,4 - 4],[0/4,G,4 - 4]).
+move_left([2/0,G,4 - 4],[1/0,G,4 - 4]).
+move_left([2/1,G,4 - 4],[1/1,G,4 - 4]).
+move_left([2/2,G,4 - 4],[1/2,G,4 - 4]).
+move_left([2/3,G,4 - 4],[1/3,G,4 - 4]).
+move_left([2/4,G,4 - 4],[1/4,G,4 - 4]).
+move_left([3/0,G,4 - 4],[2/0,G,4 - 4]).
+move_left([3/1,G,4 - 4],[2/1,G,4 - 4]).
+move_left([3/2,G,4 - 4],[2/2,G,4 - 4]).
+move_left([3/3,G,4 - 4],[2/3,G,4 - 4]).
+move_left([3/4,G,4 - 4],[2/4,G,4 - 4]).
+move_left([4/0,G,4 - 4],[3/0,G,4 - 4]).
+move_left([4/1,G,4 - 4],[3/1,G,4 - 4]).
+move_left([4/2,G,4 - 4],[3/2,G,4 - 4]).
+move_left([4/3,G,4 - 4],[3/3,G,4 - 4]).
+move_left([4/4,G,4 - 4],[3/4,G,4 - 4]).
+move_right([0/0,G,4 - 4],[1/0,G,4 - 4]).
+move_right([0/1,G,4 - 4],[1/1,G,4 - 4]).
+move_right([0/2,G,4 - 4],[1/2,G,4 - 4]).
+move_right([0/3,G,4 - 4],[1/3,G,4 - 4]).
+move_right([0/4,G,4 - 4],[1/4,G,4 - 4]).
+move_right([1/0,G,4 - 4],[2/0,G,4 - 4]).
+move_right([1/1,G,4 - 4],[2/1,G,4 - 4]).
+move_right([1/2,G,4 - 4],[2/2,G,4 - 4]).
+move_right([1/3,G,4 - 4],[2/3,G,4 - 4]).
+move_right([1/4,G,4 - 4],[2/4,G,4 - 4]).
+move_right([2/0,G,4 - 4],[3/0,G,4 - 4]).
+move_right([2/1,G,4 - 4],[3/1,G,4 - 4]).
+move_right([2/2,G,4 - 4],[3/2,G,4 - 4]).
+move_right([2/3,G,4 - 4],[3/3,G,4 - 4]).
+move_right([2/4,G,4 - 4],[3/4,G,4 - 4]).
+move_right([3/0,G,4 - 4],[4/0,G,4 - 4]).
+move_right([3/1,G,4 - 4],[4/1,G,4 - 4]).
+move_right([3/2,G,4 - 4],[4/2,G,4 - 4]).
+move_right([3/3,G,4 - 4],[4/3,G,4 - 4]).
+move_right([3/4,G,4 - 4],[4/4,G,4 - 4]).
+move_up([0/0,G,4 - 4],[0/1,G,4 - 4]).
+move_up([0/1,G,4 - 4],[0/2,G,4 - 4]).
+move_up([0/2,G,4 - 4],[0/3,G,4 - 4]).
+move_up([0/3,G,4 - 4],[0/4,G,4 - 4]).
+move_up([1/0,G,4 - 4],[1/1,G,4 - 4]).
+move_up([1/1,G,4 - 4],[1/2,G,4 - 4]).
+move_up([1/2,G,4 - 4],[1/3,G,4 - 4]).
+move_up([1/3,G,4 - 4],[1/4,G,4 - 4]).
+move_up([2/0,G,4 - 4],[2/1,G,4 - 4]).
+move_up([2/1,G,4 - 4],[2/2,G,4 - 4]).
+move_up([2/2,G,4 - 4],[2/3,G,4 - 4]).
+move_up([2/3,G,4 - 4],[2/4,G,4 - 4]).
+move_up([3/0,G,4 - 4],[3/1,G,4 - 4]).
+move_up([3/1,G,4 - 4],[3/2,G,4 - 4]).
+move_up([3/2,G,4 - 4],[3/3,G,4 - 4]).
+move_up([3/3,G,4 - 4],[3/4,G,4 - 4]).
+move_up([4/0,G,4 - 4],[4/1,G,4 - 4]).
+move_up([4/1,G,4 - 4],[4/2,G,4 - 4]).
+move_up([4/2,G,4 - 4],[4/3,G,4 - 4]).
+move_up([4/3,G,4 - 4],[4/4,G,4 - 4]).
