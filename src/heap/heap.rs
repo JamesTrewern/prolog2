8385573a937a984655390fbf09549ef8a3f0534b
James Trewern <trewern.james@gmail.com>
2026-08-12 15:15:15 +0100

    remove term equal println, add equal address shortcut

--- src/heap/heap.rs
+++ src/heap/heap.rs
@@ -270,11 +270,9 @@ pub trait Heap:
     }
 
     fn term_equal(&self, addr1: usize, addr2: usize) -> bool {
-        println!(
-            "{} =:= {}",
-            self.term_string(addr1),
-            self.term_string(addr2)
-        );
+        if addr1 == addr2{
+            return true;
+        }
         let (mut walk1, mut walk2) = (TermWalk::new(addr1), TermWalk::new(addr2));
         loop {
             let (Some(cell1), Some(cell2)) = (walk1.next_cell(self), walk2.next_cell(self)) else {
